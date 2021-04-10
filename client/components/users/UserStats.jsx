664103e880e0073a1e992b7a8780d782c3e56f14
Michael Kolarik <mikekolarik@gmail.com>
2021-04-10 10:04:48 -0500

    Render unformatted stats to UserStats component

--- client/components/users/UserStats.jsx
+++ client/components/users/UserStats.jsx
@@ -1,7 +1,26 @@
+import React, { useState, useEffect } from "react";
 import { Grid, Card, CardContent, CardHeader } from "@material-ui/core";
-import React from "react";
+import { getOverallStats } from "../../../api_master";
 
-function UserStats() {
+function UserStats({ loggedInUser }) {
+  const [stats, setStats] = useState({});
+
+  var refreshStats = () => {
+    if (loggedInUser.id) {
+      getOverallStats(loggedInUser.id).then((stats) => {
+        setStats(stats.data);
+      });
+    }
+  };
+  refreshStats = refreshStats.bind(this);
+
+  useEffect(() => {
+    refreshStats();
+    return () => {
+      setStats({});
+    };
+  }, []);
+
   return (
     <Grid item xs>
       <Card style={{ height: "100%" }}>
@@ -10,7 +29,14 @@ function UserStats() {
           titleTypographyProps={{ variant: "body2" }}
           style={{ padding: 10 }}
         />
-        <CardContent></CardContent>
+        <CardContent>
+          <p>{`Completed Quizzes ${stats.completedQuizzes}`}</p>
+          <p>{`Correct Answers ${stats.correctAnswers}`}</p>
+          <p>{`Avg Score ${stats.userAvgScore}`}</p>
+          <p>{`Avg Completed Quizzes ${stats.globalAvgQuizzesComp}`}</p>
+          <p>{`Avg Correct Answers ${stats.globalAvgCorrectAnswers}`}</p>
+          <p>{`Avg Avg Score ${stats.globalAvgScore}`}</p>
+        </CardContent>
       </Card>
     </Grid>
   );
