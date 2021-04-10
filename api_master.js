fae4c27546e90ad0985711e033410baf1110a7b7
Michael Kolarik <mikekolarik@gmail.com>
2021-04-09 19:33:42 -0500

    Add GlobalRankings api call

--- api_master.js
+++ api_master.js
@@ -63,6 +63,11 @@ function getQuizFriendRankings(quizId, userId) {
   return handleGetRequests(`/quiz/rankings/friends/${quizId}/${userId}`);
 }
 
+function getOverallRankings(userId, userPool, rankingType) {
+  let data = { userId: userId, userPool: userPool, rankingType: rankingType };
+  return handleGetRequests(`/users/ranking`, data);
+}
+
 // Handles all POST requests, takes a route, params, and data object
 function handlePostRequests(route, data = {}, params = {}) {
   let options = {
@@ -137,6 +142,7 @@ export {
   getQuizHistory,
   getQuizGlobalRankings,
   getQuizFriendRankings,
+  getOverallRankings,
   getRandomQuizzes,
   getSelectQuizzes,
   getSingleQuiz,
