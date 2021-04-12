81dbd3045a268d1d5777b53554919e1d484429ee
Michael Kolarik <mikekolarik@gmail.com>
2021-04-11 20:23:26 -0500

    Change port back to 3000

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM node:lts-alpine
 WORKDIR /app
 COPY . .
 RUN npm install && npm run-script build
-EXPOSE 5000
+EXPOSE 3000
 CMD ["node", "server/index.js"]
--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { rankingQueryMaker } = require("../helperFunctions.js");
 
 require("dotenv").config();
 
-const port = 5000;
+const port = 3000;
 const app = express();
 
 app.use(express.json());
